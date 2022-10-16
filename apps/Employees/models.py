d3102618c92fbcc0c102c04603bb578001352887
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-10-16 14:42:34 -0700

    Employees App models updated!

--- apps/Employees/models.py
+++ apps/Employees/models.py
@@ -1,3 +1,63 @@
+from email.policy import default
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+# |========================================|
+# |==========| EMPLOYEES MODELS |==========|
+# |========================================|
+
+# |----------| MODEL #1: Employee |----------|
+class Employee(models.Model):
+    empFirstName:       models.CharField(max_length=50, null = False)
+    empLastName:        models.CharField(max_length=50, null = False)
+    empBirthDate:       models.DateField(null = False)
+    empDateJoined:      models.DateField(auto_now_add=True, null = False)
+    empStatus:          models.BooleanField(default=False)
+    empUsername:        models.OneToOneField(User, on_delete=models.CASCADE, null = False)
+    empPassword:        models.CharField(max_length=50, null = False)
+    empLastLogin:       models.DateTimeField(auto_now=True, null = False)
+    
+    def __str__(self):
+        return self.empName
+
+# |----------| MODEL #2: Department |----------|
+class Department(models.Model):
+    deptName:           models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+# |----------| MODEL #3: job_Position |----------|
+class job_Position(models.Model):
+    jpName:             models.CharField(max_length=50, null = False)
+    jpEmail:            models.EmailField(max_length=50, null = False)
+    
+    # -----> Foreign Keys <----- #
+    # Foreign Key that points to the Department model
+    jpDepartment:       models.ForeignKey(Department, on_delete=models.CASCADE, null = False)
+
+# |----------| MODEL #4: Worker |----------|
+class Worker(models.Model):
+    workerSalary:       models.DecimalField(max_digits=10, decimal_places=2, null = False)
+    workerEntrance:     models.TimeField(null = False)
+    workerOut:          models.TimeField(null = False)
+    
+    # -----> Foreign Keys <----- #
+    # Foreign Key that points to the Employee model
+    workerEmployee:     models.ForeignKey(Employee, on_delete=models.CASCADE, null = False)
+    # Foreign Key that points to the job_Position model
+    workerJobPosition:  models.ForeignKey(job_Position, on_delete=models.CASCADE, null = False)
+    
+    def __str__(self):
+        return self.workerEmployee.empName
+
+class Assistence(models.Model):
+    assistEntrance:     models.TimeField(null = False)
+    assistOut:          models.TimeField(null = False)
+    
+    # -----> Foreign Keys <----- #
+    # Foreign Key that points to the Worker model
+    assistWorkr:        models.ForeignKey(Worker, on_delete=models.CASCADE, null = False)
