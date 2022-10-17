7996098b5427bdd84a5ca35f0e21425252da367b
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-10-17 12:26:30 -0700

    Finished decorators file!

--- apps/Employees/decorators.py
+++ apps/Employees/decorators.py
@@ -26,3 +26,18 @@ def allowed_users(allowed_roles=[]):
 				return HttpResponse('You are not authorized to view this page')
 		return wrapper_func
 	return decorator
+
+# Función o decorador que solo permite a los administradores acceder a ciertas páginas
+def admin_only(view_func):
+	def wrapper_function(request, *args, **kwargs):
+		group = None
+		if request.user.groups.exists():
+			group = request.user.groups.all()[0].name
+
+		if group == 'employee':
+			return redirect('dashboard_employee')
+
+		if group == 'admin':
+			return view_func(request, *args, **kwargs)
+
+	return wrapper_function
