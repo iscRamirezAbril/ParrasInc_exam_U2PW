ff3e3d321721acb43df31726fb6aed6c4d14853f
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-10-20 14:30:47 -0700

    URLS and views configuration

--- apps/API/views.py
+++ apps/API/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
+from urllib import response
 from django.shortcuts import render
 
-# Create your views here.
+# |-----| All the necessary Libs |-----|
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# |-----| Views |-----|
+@ api_view(['GET'])
+def getAssistenceList(request):
+    context = {'name': 'Abril', 'age': 21}
+    return response(context)
