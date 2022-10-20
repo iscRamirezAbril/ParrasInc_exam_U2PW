9c43b91c26926128ed7a0864032802af2ca9ff42
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-10-19 18:56:43 -0700

    Report of areas employees in progress!

--- apps/Production/views.py
+++ apps/Production/views.py
@@ -3,20 +3,17 @@ from apps.Production.models import *
 # Create your views here.
 
 # |===============| ADMINISTRACIÓN DE PRODUCCIÓN |===============| #
-# LISTO #
-# Lista de empleados por áreas #
-# def worker_Area(request):
-#     auto = Area_Worker.objects.filter(areaWorkName = 1)
-#     microTec = Area_Worker.objects.filter(areaWorkName = 2)
-        
-#     return render(request, 'Production/worker_areaList.html', {'auto': auto, 'microTec': microTec})
-
 # Lista de empleados por áreas #
 def worker_Area(request):
-    areas = Area.objects.all()
-    context = {'areasList': areas}
+    areas = Area.objects.all() # Obtiene todas las áreas
+    workers = LineMember.objects.all() # Obtiene todos los empleados
+    lines = ProductionLine.objects.all() # Obtiene todas las líneas de producción
+    context = {'areasList': areas,
+               'workersList': workers,
+               'linesList': lines} # Crea un diccionario con los datos
     
     return render(request, 'Production/worker_areaList.html', context)
 
+# Dashboard de producción #
 def dashboard_Production(request):
     return render(request, 'Production/dashProduction.html')
